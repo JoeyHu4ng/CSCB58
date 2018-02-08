5d3d24f2f31339b6950c18a585292e6423f6730f
Joey Huang <joeyhuang921@outlook.com>
2018-02-08 11:53:01 -0500

    deleting

--- week2/3-bit_mux2.v
+++
@@ -1,8 +0,0 @@
-module mux(X, Y, S, M);
-   parameter N = 3;
-   input [N:1] X, Y;
-   input       S;
-   output [N:1] M;
-
-   assign M = S ? Y : X;
-endmodule // mux
--- week2/mux.v
+++
@@ -1,6 +0,0 @@
-module mux(X, Y, S, M);
-   input X, Y, S;
-   output M;
-
-   assign M = X & ~S | Y & S;
-endmodule // mux
